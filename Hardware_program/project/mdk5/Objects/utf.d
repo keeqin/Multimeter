0cad6c53d76c0882e284ff0d15e7b1f173050a28
keeqin <qk2313163237@gmail.com>
2021-12-12 20:17:23 +0800

    重构目录结构

--- Hardware_program/project/mdk5/Objects/utf.d
+++
@@ -1,4 +0,0 @@
-.\objects\utf.o: ..\..\external\utf_converter\utf.c
-.\objects\utf.o: ..\..\external\utf_converter\utf.h
-.\objects\utf.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\objects\utf.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
